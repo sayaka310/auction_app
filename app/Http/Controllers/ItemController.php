915fe58d3a86220caa09408530506eb70321c2e6
sayaka310 <syk3887@yahoo.co.jp>
2021-09-18 12:20:26 +0900

    登録機能完成

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -17,6 +17,34 @@ class ItemController extends Controller
         return view('items.index', ['items' => $items]);
     }
 
+    public function create()
+    {
+        return view('items.create');
+    }
+
+    public function store(Request $request)
+    {
+        // インスタンスの作成
+        $item = new Item;
+
+        // 値の用意
+        $item->name = $request->name;
+        $item->description = $request->description;
+        $item->price = $request->price;
+        $item->seller = $request->seller;
+        $item->email = $request->email;
+        $item->image_url = $request->image_url;
+
+        // インスタンスに値を設定して保存
+        $item->save();
+
+        // 登録したらindexに戻る
+        return redirect('/items');
+    }
+        //
+
+
+
     // showページへ移動
     public function show($id)
     {
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -7,12 +7,15 @@
 <title>auction index</title>
 </head>
 <body>
-<h1>出品商品一覧</h1>
+    <h1>出品商品一覧</h1>
     <ul>
         @foreach ($items as $item)
             <!-- // リンク先をidで取得し名前で出力 -->
             <li><a href="/items/{{ $item->id }}">{{ $item->name }}</a></li>
         @endforeach
     </ul>
-</body>
+
+    <!-- 新規登録画面へジャンプする -->
+    <a href="/items/create">出品する</a>
+</body> 
 </html>
